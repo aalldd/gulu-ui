75d0f694ee3f3964440ba575e71f21694085bc50
Daniel <740907068@qq.com>
2020-12-26 10:22:16 +0800

    基本实现了row和col的功能

--- src/row.vue
+++ src/row.vue
@@ -1,8 +1,24 @@
 <template>
-  <div class="row">
+  <div class="row" :style="{marginLeft:-gutter/2+'px',marginRight:-gutter/2+'px'}">
     <slot></slot>
   </div>
 </template>
+
+<script>
+export  default {
+  props:{
+    gutter:{
+      type:[Number,String]
+    }
+  },
+  mounted(){
+    console.log(this.$children);
+    this.$children.forEach(item=>{
+      item.gutter=this.gutter
+    })
+  }
+}
+</script>
 
 <style scoped lang="scss">
 .row{
