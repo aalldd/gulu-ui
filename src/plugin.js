b787bdeca035f56a4690c64c1f79ecbeac6d98b2
Daniel <740907068@qq.com>
2020-12-27 16:21:10 +0800

    实现了只能有一个toast的功能

--- src/plugin.js
+++ src/plugin.js
@@ -1,17 +1,28 @@
 //为什么要用plugin，1.避免吧vue配到另外一个轮子上
 // 2.$toast可能会覆盖现有的东西，但我们不确定，所以让用户自己去覆盖
 import Toast from './toast'
+//我们不让同时出现两个相同的toast
+
+let currentToast
 export default {
-    install(Vue,options){
-        Vue.prototype.$toast=function (message,toastOptions){
-            let Constructor =Vue.extend(Toast)
-            let toast=new Constructor({
-                propsData:toastOptions
-            })
-            //slot要放在mount前面
-            toast.$slots.default=[message]
-            toast.$mount()
-            document.body.appendChild(toast.$el)
+    install(Vue, options) {
+        Vue.prototype.$toast = function (message, toastOptions) {
+            if(currentToast){
+                currentToast.close()
+            }
+            currentToast=createToast({Vue, message, propsData: toastOptions})
         }
     }
+}
+//helpers
+let createToast = ({Vue, message, propsData}) => {
+    let Constructor = Vue.extend(Toast)
+    let toast = new Constructor({
+        propsData: propsData
+    })
+    //slot要放在mount前面
+    toast.$slots.default = [message]
+    toast.$mount()
+    document.body.appendChild(toast.$el)
+    return toast
 }
